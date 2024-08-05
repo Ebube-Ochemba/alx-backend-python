b195bbe5c4715e9a7ef65c83a694641814ec40ca
Ebube-Ochemba <ebube17@gmail.com>
2024-08-05 13:16:35 +0100

    updated specs

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """"A module that return a tuple"""
 
+from typing import Union, Tuple
 
-def to_kv(k: str, v: int | float) -> tuple:
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Function that takes a string and an int/float and returns a tuple"""
     return (k, v**2)
