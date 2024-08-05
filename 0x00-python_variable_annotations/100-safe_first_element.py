0059eff142e12aa512c9d74589f155e06aa29789
Ebube-Ochemba <ebube17@gmail.com>
2024-08-05 14:38:02 +0100

    NoneType

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""returns the first element of a list"""
+from typing import Any, Sequence, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
