ebecd5580753f3424ac3c27e14ddc34795c193ad
Ebube-Ochemba <ebube17@gmail.com>
2024-08-05 13:12:12 +0100

    updated variables

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,6 +2,6 @@
 """"A module that return a tuple"""
 
 
-def to_kv(key: str, value: int | float) -> tuple:
+def to_kv(k: str, v: int | float) -> tuple:
     """Function that takes a string and an int/float and returns a tuple"""
-    return (key, value**2)
+    return (k, v**2)
