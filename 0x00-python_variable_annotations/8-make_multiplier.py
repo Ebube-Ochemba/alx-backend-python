06b62e1f53ae1191a98109ba68ae12c9cd341703
Ebube-Ochemba <ebube17@gmail.com>
2024-08-05 14:03:27 +0100

    done

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A function that returns a function"""
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Return a function that multiplies a float by multiplier"""
+    return lambda x: x * multiplier
