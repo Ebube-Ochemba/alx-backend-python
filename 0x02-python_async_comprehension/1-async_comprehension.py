73beb35699785d81fe31099a34a86a67f0bfd927
Ebube-Ochemba <ebube17@gmail.com>
2024-08-07 19:03:04 +0100

    import style

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 """An Async Comprehension"""
-
 from typing import List
-from importlib import import_module
 
-async_generator = import_module('0-async_generator').async_generator
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
